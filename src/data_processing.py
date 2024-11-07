daad43f846eec92467e2b0c61fdc0a7a9e03fef6
Samuel-Emeus-Jun <22030407@upgto.edu.mx>
2024-11-07 15:18:02 -0600

    Date Format

--- src/data_processing.py
+++ src/data_processing.py
@@ -2,15 +2,14 @@
 ##DE PROCESAMIENTO DE DATOS
 import pandas as pd
 
-df = pd.read_csv('data/Satisfacción de servicio para UPG 2024.csv', header = [0,1])
+def unir_string_headers():
+    df.columns = [
+        f"{str(col1).strip()}_{str(col2).strip()}" if pd.notna(col1) and pd.notna(col2)
+        else str(col1) if pd.notna(col1)
+        else str(col2)
+        for col1, col2 in df.columns
+    ]
 
-
-df.columns = [
-    f"{str(col1).strip()}_{str(col2).strip()}" if pd.notna(col1) and pd.notna(col2)
-    else str(col1) if pd.notna(col1)
-    else str(col2)
-    for col1, col2 in df.columns
-]
 
 ##Mostrar el DataFrame con los encabezados más legibles
 #print(df.head())
@@ -20,13 +19,26 @@ df.columns = [
 # headers = [col for col in df.columns]
 # print(headers_with_index)
 # print(headers)
-print(df.iloc[:, 40])
 
-def drop_na_rows(df, start_col, end_col):
-    cols_to_check = df.columns[start_col:end_col + 1]
-    df_cleaned = df.dropna(subset=cols_to_check)
-    return df_cleaned
 
-df = drop_na_rows(df, 37, 40)
-df = drop_na_rows(df, 42, 43)
+###GRIT 150: Funciones para limpieza tosca del df
 
+#Funcion para droppear filas con valores nulos
+def grit_150(dataframe):
+
+    
+    df = dataframe.dropna(subset=dataframe.columns[37:40+1])
+    df = dataframe.dropna(subset=dataframe.columns[42:43+1])
+    #dataframe.iloc[:, 3] = pd.to_datetime(dataframe.iloc[:, 3], errors='coerce')
+    df['Fecha'] = pd.to_datetime(dataframe.iloc[:, 3], format = 'mixed', dayfirst = False).dt.date
+
+    return df
+    
+
+df = pd.read_csv('data/Satisfacción de servicio para UPG 2024.csv', header = [0,1])
+
+unir_string_headers()
+df = grit_150(df)
+
+print(df.iloc[:, 3])
+print(df['Fecha'])
