5358e73d642a856c8741d9300baf3778e28d1367
Samuel-Emeus-Jun <22030407@upgto.edu.mx>
2024-11-10 09:44:55 -0600

    Mappeando nueva DF

--- src/data_processing.py
+++ src/data_processing.py
@@ -26,12 +26,32 @@ def limpiar_dataframe(dataframe):
     ##Generar columnas cuantitativas a partir de columnas cualitativas
     map_dict = {"Pésimo" : 0, "Muy Malo" : 1, "Malo" : 3, "Regular - Malo" : 5,
                 "Regular - Bueno" : 6, "Bueno" : 8, "Muy Bueno" : 9, "Excelente" : 10}
-    df['Atencion'] = df.iloc[:, 37].map(map_dict)
+    df['Atención brindada'] = df.iloc[:, 37].map(map_dict)
     df['Profesionalismo'] = df.iloc[:, 38].map(map_dict)
-    df['Tiempo de Entrega'] = df.iloc[:, 39].map(map_dict)
-    df['Calidad del Producto'] = df.iloc[:, 40].map(map_dict)
+    df['Tiempo de entrega'] = df.iloc[:, 39].map(map_dict)
+    df['Calidad del producto'] = df.iloc[:, 40].map(map_dict)
     
     return df
+
+def mappear_df(dataframe):
+
+    mapped_df = pd.DataFrame()
+    mapped_df = pd.concat([mapped_df, dataframe['Fecha']], axis =1)
+    mapped_df = pd.concat([mapped_df, dataframe.iloc[:, 10].rename("Nombre de la Compañía")], axis =1)
+    for col in dataframe.iloc[:, 16 : 35+1]:
+        mapped_df = pd.concat([mapped_df, dataframe[col].rename(dataframe[col].name[1])], axis =1)
+    # for col in dataframe.iloc[:, 37:40+1]:
+    #     mapped_df = pd.concat([mapped_df, dataframe[col].rename(dataframe[col].name[1])], axis =1)
+    mapped_df = pd.concat([mapped_df, dataframe['Atención brindada']], axis =1)
+    mapped_df = pd.concat([mapped_df, dataframe['Profesionalismo']], axis =1)
+    mapped_df = pd.concat([mapped_df, dataframe['Tiempo de entrega']], axis =1)
+    mapped_df = pd.concat([mapped_df, dataframe['Calidad del producto']], axis =1)
+    for col in dataframe.iloc[:, 42:43+1]:
+        mapped_df = pd.concat([mapped_df, dataframe[col].rename(dataframe[col].name[1])], axis =1)
+    mapped_df = pd.concat([mapped_df, dataframe.iloc[: , 44].rename("Comentarios")], axis =1)
+
+    return mapped_df
+
 
 
 ##FIltrar df obtenido de limpiar_df a 12 meses    
@@ -50,13 +70,14 @@ df = pd.read_csv('data/Satisfacción de servicio para UPG 2024.csv', header = [0
 
 
 df = limpiar_dataframe(df)
+df_mappeada = mappear_df(df)
 
 
 ##VISORES
 
 #print(df.iloc[:, 3])
 #print(df.iloc[:, 16].to_list())
-print(df['Fecha'])
+#print(df['Fecha'])
 #print(df.xs('Ana Aguirre', axis=1, level=1))
 #print(df['Unnamed: 36_level_0']['Otro (especifique)'])
 #print(colaboradores)
