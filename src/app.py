0836a8cb2c24fefac938e6e625bc80968f898cd0
Samuel-Emeus-Jun <22030407@upgto.edu.mx>
2024-11-16 20:57:21 -0600

    reporte_anual.html listo para prueba

--- src/app.py
+++ src/app.py
@@ -5,8 +5,8 @@ import pandas as pd
 import plotly as plt
 import plotly.express as px
 from data_processing import limpiar_dataframe, mappear_df
-from plot_generator import evaluacion_desempeño
-from utils import generar_lista_de_colaboradores
+from plot_generator import evaluacion_desempeño, generar_donas
+from utils import generar_lista_de_colaboradores, cleanup_static
 
 app = Flask(__name__)
 
@@ -40,32 +40,42 @@ def upload_file():
 
     # Verificar que el archivo sea un .csv
     if file and file.filename.endswith('.csv'):
+
+        ##AQUI SE DEBE AGREGAR LA PANTALLA DE CARGA
+        render_template('cargando.html')
+
         # Leer el archivo CSV directamente en un DataFrame
         df = pd.read_csv(file)
         df_limpia = limpiar_dataframe(df)
         colaboradores = generar_lista_de_colaboradores(df_limpia)
         df_global = mappear_df(df_limpia)
 
-        # Generar reporte general
+        ##FUNCIÓN DE LIMPIEZA DE ARCHIVOS EN STATIC
+        #cleanup_static()
+
+        # GENERAR REPORTE GENERAL
+
         tipo_de_reporte = "general"
         servicios_minimos = 5
+
         ##FUNCIÓN DE BARRAS
+
+        ##FUNCIÓN DONAS
+        generar_donas(df_global, tipo_de_reporte)
 
         ##FUNCIÓN DE EVALUACIÓN DE DESEMPEÑO
         for colaborador in colaboradores:
             temp_df = df_global[df_global[colaborador] == colaborador]
             if len(temp_df) >= servicios_minimos:
                 evaluacion_desempeño(temp_df, colaborador, tipo_de_reporte)
-                
 
-    
-
+        ##FUNCIÓN DE TEXTO
 
         
         # Verificar si carga correctamente el archivo en un df
         #print(df)
             
-        # Continuar con el flujo de datos (por ahora solo redirigimos al reporte anual)
+        # REDIRIGE A REPORTE GENERAL
         return redirect(url_for('reporte_general'))
     
     return 'El archivo debe ser un archivo CSV (.csv)'
@@ -74,8 +84,16 @@ def upload_file():
 # Ruta para el Reporte General
 @app.route('/general')
 def reporte_general():
-    ##print(evaluaciones)
-    return render_template('reporte_general.html')
+    global barras, donas, evaluaciones, texto, colaboradores
+
+    
+    return render_template('reporte_general.html',
+                           tipo_de_reporte="general",
+                           barras=barras,
+                           donas=donas,
+                           evaluaciones=evaluaciones,
+                           texto=texto,
+                           colaboradores=colaboradores)
 
 # Ruta para el Reporte Anual
 @app.route('/anual')
